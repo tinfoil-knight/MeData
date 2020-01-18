c9bfb723865412a18e831b1dbba407a741ca8802
tinfoil-knight <kunal99kundu@gmail.com>
2020-01-18 10:10:57 +0530

    Genesis Block Creation Added

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -1,4 +1,10 @@
 const SHA256 = require('crypto-js/sha256')
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
+const myKey = ec.keyFromPrivate('ee4442373a701cdf97dc60155a008b74377ea849e3386df18b04989c631238ec')
+const myWalletAddress = myKey.getPublic('hex')
+const otherKey = ec.keyFromPrivate('d1ec6c7bacbe853c57cf9659bae5d871dc6307e3623ef4a89662aa854438dc79')
+const otherWalletAddress = otherKey.getPublic('hex')
 
 class Update {
   constructor(fromAddress, toAddress, objHash, token) {
@@ -60,7 +66,7 @@ class Blockmanager {
   }
 
   createGenesisBlock() {
-    return // TODO: Enter the manual genesis block here
+    return new Block(Date.parse('2017-01-01', [], 0))
   }
 
   getLatestBlock() {
@@ -68,7 +74,7 @@ class Blockmanager {
   }
 
   minePendingUpdates(miningRewardAddress) {
-    const rewardTx = new Update(null, miningRewardAddress, this.miningReward)
+    const rewardTx = new Update(null, miningRewardAddress, 'todo' ,this.miningReward)
     this.pendingUpdates.push(rewardTx)
 
     const block = new Block(Date.now(), this.pendingUpdates, this.getLatestBlock().hash)
@@ -87,4 +93,40 @@ class Blockmanager {
 
     this.pendingUpdates.push(update)
   }
+}
+
+// TESTING
+console.log("Initialisation of blockchain")
+let EHRChain = new Blockmanager()
+
+
+
+// Remember that you have to use the patient key and not the hospital key to sign things
+//(Why though? Because only a patient should be allowed to add data)You changed it above.
+// Make a update
+console.log("Creating Update 1")
+const up1 = new Update(myWalletAddress, otherWalletAddress, 'ghf022002be4ea13d279f75ec302b88a2ea467fc85748336', 10)
+//signing of update will only be done by Hospital Key and notpatient key because thats idiotic
+//too much distrust is what you have
+//up1.signUpdate(otherKey)
+//sending the update to an array waiting to be mined
+EHRChain.addUpdate(up1)
+// You'll only be dealing with one patient at a time(But there are multiple receptionists! So.....)
+console.log("Creating Update 2")
+// log store SQL attempt here
+const up2 = new Update(otherWalletAddress, myWalletAddress, 'aa371b46d1dd5234ed46c3d9e9f2e60d44cdd2ee8656bf25', 5)
+// no signing of updates will be done
+//up2.signUpdate(myKey)
+EHRChain.addUpdate(up2)
+// log change value of SQL attempt to passed(or shift this to after mining updates)
+
+// Mine block
+console.log("Start mining")
+EHRChain.minePendingUpdates(myWalletAddress)
+console.log("End mining")
+console.log(JSON.stringify(EHRChain.chain, null, 4))
+module.exports = {
+  Update: Update,
+  Block: Block,
+  Blockchain: Blockmanager
 }
